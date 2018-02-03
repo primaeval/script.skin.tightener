c91f246294c082a28e010aa38c9267f49833b1c3
primaeval <primaeval@users.noreply.github.com>
2018-02-03 12:35:03 +0100

    enable

--- addon.py
+++ addon.py
@@ -1,9 +1,17 @@
 import xbmc,xbmcaddon,xbmcvfs,xbmcgui
 import re
+import re, os, sys
+import xbmc, xbmcgui, xbmcplugin, xbmcvfs, xbmcaddon
+import time
+
+if sys.version_info < (2, 7):
+    import simplejson as json
+else:
+    import json
+
 
 def log(x):
-    xbmc.log(repr(x))
-d = xbmcgui.Dialog()
+    xbmc.log(repr(x),xbmc.LOGERROR)
 
 def copyTree(src,dst):
     #log((src,dst))
@@ -19,12 +27,14 @@ def copyTree(src,dst):
                 xml = re.sub('time=".*?"','time="0"',xml)
                 xml = re.sub(r'(<scrolltime.*?>)(.*?)(</scrolltime>)',r'\g<1>0\g<3>',xml)
                 xml = re.sub(r'(fadetime.*?>)(.*?)(</fadetime>)',r'\g<1>0\g<3>',xml)
-            xml = re.sub(re.escape(old_skin),new_skin,xml) 
+            xml = re.sub(re.escape(old_skin),new_skin,xml)
             xml = re.sub(re.escape(old_skin_name),new_skin_name,xml)
             xbmcvfs.File(dst+f,'wb').write(xml)
         else:
             xbmcvfs.copy(src+f,dst+f)
-    
+
+
+d = xbmcgui.Dialog()
 
 location = d.select("Choose Skin Location", ["System Skin", "Installed Skin"])
 if location == -1:
@@ -41,7 +51,7 @@ if not new_skin:
     quit()
 new_path = 'special://home/addons/%s/' % new_skin
 if xbmcvfs.exists(new_path):
-    ok = d.ok('Folder already exists', 'Overwrite %s ?' % new_path)
+    ok = d.yesno('Folder already exists', 'Overwrite %s ?' % new_path)
     if not ok:
         quit()
 
@@ -49,4 +59,21 @@ xml = xbmcvfs.File(old_path+'addon.xml','rb').read()
 old_skin_name = re.search('<addon.*?name="(.*?)"',xml,flags=(re.DOTALL | re.MULTILINE)).group(1)
 new_skin_name = d.input('New Skin Name (%s)' % old_skin_name, old_skin_name+' Fast')
 copyTree(old_path,new_path)
-d.ok('Skin Tightener','Restart Kodi and Select "%s" as Skin' % new_skin_name)
+
+xbmc.executebuiltin("UpdateLocalAddons")
+xbmc.executebuiltin("UpdateAddonRepos")
+time.sleep(2)
+params = '"method":"Addons.SetAddonEnabled","params":{"addonid":"%s","enabled":true}'% new_skin
+try:
+    xbmc.executeJSONRPC('{"jsonrpc": "2.0", %s, "id": 1}' % params)
+    xbmc.executebuiltin("ActivateWindow(appearancesettings)")
+except:
+    d.ok("Skin Tightener","Kodi web interface wasn't enabled. Restart Kodi and Enable Skin in My Addons.")
+
+
+
+
+
+
+
+
